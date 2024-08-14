12d6f8f38036a5f670e55c01a8d2e73679398dcd
路人甲 <jonaszhang91@gmail.com>
2024-08-15 02:25:14 +0800

    install sh

---
+++ test/installport.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 定义下载文件的URL和本地保存路径
+file_url="https://github.com/jonaszhang91/portService/raw/main/test/protTest"
+local_path="/home/menu/protTest"
+
+# 使用wget命令下载文件
+wget "$file_url" -O "$local_path"
+
+# 检查wget命令的退出状态，‌确保文件已成功下载
+if [ $? -eq 0 ]; then
+    # 修改下载文件的权限为700
+    chmod 700 "$local_path"
+    echo "文件下载并修改权限成功！‌"
+    /home/menu/protTest
+else
+    echo "文件下载失败！‌"
+fi
